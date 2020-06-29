f11433310dea9728942a966d357375dbfec50303
heyimmatt <mrmcfarland@gmail.com>
2020-06-29 11:47:38 -0700

    Answer object destructuring questions

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+//object destructuring 1
+//8
+//1846
+
+//object destructuring 2
+//{ yearNeptuneDiscovered: 1846,
+//  yearMarsDiscovered: 1659
+//}
+
+//object destructuring 3
+// `Your name is Alejandro and you like purple`
+// `Your name is Melissa and you like green`
+// `Your name is undefined and you like green`
