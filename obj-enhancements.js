79678d2989e42de5404ebf11a16cb0a3ddbb0f00
heyimmatt <mrmcfarland@gmail.com>
2020-06-29 11:02:34 -0700

    Completes parts 1-3 of exercise

---
+++ obj-enhancements.js
@@ -0,0 +1,49 @@
+//same keys and values
+// function createInstructor(firstName, lastName){
+//   return {
+//     firstName: firstName,
+//     lastName: lastName
+//   }
+// }
+
+const createInstructor = (firstName, lastName) => {
+  return {
+    firstName,
+    lastName
+  }
+}
+
+//computed property names
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+const favoriteNumber = 42;
+const instructor = {
+  firstName: 'Colt',
+}
+instructor[favoriteNumber] = 'That is my favorite!';
+
+//Object Methods
+// var instructor = {
+//   firstName: "Colt",
+//   sayHi: function(){
+//     return "Hi!";
+//   },
+//   sayBye: function(){
+//     return this.firstName + " says bye!";
+//   }
+// }
+const instructor = {
+  firstName: 'Colt',
+  sayHi() {
+    return 'Hi!';
+  },
+  sayBye() {
+    return this.firstName + ' says bye!';
+  }
+}
