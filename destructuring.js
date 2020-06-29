be7c65f9290e656bb0de7c32313fc24e4b4ba752
heyimmatt <mrmcfarland@gmail.com>
2020-06-29 11:50:57 -0700

    Answer array destructuring questions

--- destructuring.js
+++ destructuring.js
@@ -11,3 +11,16 @@
 // `Your name is Alejandro and you like purple`
 // `Your name is Melissa and you like green`
 // `Your name is undefined and you like green`
+
+//array destructuring 1
+// Maya
+// Marisa
+// Chi
+
+//array destructuring 2
+// Raindrops on roses
+// whisekers on kittens
+// ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+//array destructuring 3
+// [10, 30, 20]
