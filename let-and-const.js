9dd87fce7a31a1c9a42d5038cd20ff30998b2bfc
heyimmatt <mrmcfarland@gmail.com>
2020-06-29 08:47:48 -0700

    Complete the let and const exercise

---
+++ let-and-const.js
@@ -0,0 +1,13 @@
+const PI = 3.14;
+
+// Diff between Var and Let
+// Var is hoisted. Let is block scoped. Var can be reassigned and redclared. Let can only be reassigned.
+
+// Diff between var and const
+// Const cannot be reassigned or redeclared like Var can. Const is block scoped.
+
+// Diff between let and const
+// Let can be reassigned whereas Const cannot.
+
+// Hoisting:
+// During compiling, functions and variables are lifted to the top of their scope. 
