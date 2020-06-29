a95ef649721b7933b6d53883b9d29efecded279f
heyimmatt <mrmcfarland@gmail.com>
2020-06-29 12:47:06 -0700

    Complete the exercise

--- maps-and-sets.js
+++ maps-and-sets.js
@@ -7,3 +7,22 @@
 //QQ 3
 // 0: {Array(3) => true}
 // 1: {Array(3) => false}
+
+const hasDuplicate = (arr) => {
+  const set = new Set([...arr]);
+  return arr.length === set.size ? false : true;
+};
+
+const vowelCount = (str) => {
+  const vowels = 'aeiou';
+  const vowelMap = new Map();
+  for (let char of str) {
+    let lowerCaseChar = char.toLowerCase();
+    if (vowels.includes(lowerCaseChar)) {
+      vowelMap.has(lowerCaseChar)
+        ? vowelMap.set(loweCaseChar, vowelMap.get(lowerCaseChar) + 1)
+        : vowelMap.set(lowerCaseChar, 1);
+    }
+  }
+  return vowelMap
+};
